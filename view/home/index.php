381290d6297d89427f2a781e3e080684357fc6f5
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-04-21 14:19:53 +0200

    Removed the "Welcome, (User name)" section from homepage

--- view/home/index.php
+++ view/home/index.php
@@ -1,6 +1,2 @@
 <div id="Container">
-    <h1 class="Welcome">Welcome, <br>
-    <?php if(!empty($_SESSION['isAdmin'])){ ?> Admin
-	<?php } ?><?= $_SESSION['Firstname'].' '.$_SESSION['Lastname'] ?></h1>
-    <br/>
 </div>
