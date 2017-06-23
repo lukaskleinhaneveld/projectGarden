1bdbc6ef2b2e598ad22a5ceff4df88037d2d4f8d
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-06-23 11:18:26 +0200

    no message

--- view/templates/header.php
+++ view/templates/header.php
@@ -12,8 +12,12 @@
 	<link rel="stylesheet" href="<?= URL ?>css/jquery-ui.min.css">
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+
+  	<script type="text/javascript" src="//code.jquery.com/jquery-1.12.4.js"></script>
+  	<script type="text/javascript" src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script> -->
 </head>
 <body>
 	<div id="MenuOuter">
